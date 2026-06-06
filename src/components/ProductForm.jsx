9425d7f4ee08f308d28977f586e5144fce4985ac
IRENE <irenerutto@gmail.com>
2026-06-07 00:21:38 +0300

    added comments to ProductForm

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -2,25 +2,26 @@ import { useState } from "react";
 import styles from "./ProductForm.module.css";
 
 function ProductForm({ onSubmit }) {
+   // State to store all form input values
   const [form, setForm] = useState({
     name: "",
     description: "",
     origin: "",
     price: "",
   });
-
+  // Updates state whenever an input field changes
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
-
+// Handles form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-
+  // Send data to parent component
     onSubmit({
       ...form,
       price: Number(form.price),
     });
-
+ // Reset form after submission
     setForm({
       name: "",
       description: "",
